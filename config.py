2cf888bd62bed4ad6c34aa34a2dff234b7389d6a
Andrey <andrey@zavrichko.dev>
2024-08-15 20:28:47 +0300

    hw19

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+context = os.getenv('context', 'bstack')
+bstack_userName = os.getenv('bstack_userName', 'andreyzavrichko_Xe3Bnj')
+bstack_accessKey = os.getenv('bstack_accessKey', 'jhEPjqxKLx9dxaDjMSCk')
